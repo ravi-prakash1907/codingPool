0cf41a7a911945b12da550e831dc3958b89710fd
Ravi Prakash <1907prakash.ravi@gmail.com>
2021-11-01 15:28:45 +0530

    Update maxOfTwo.py

--- python/maxOfTwo.py
+++ python/maxOfTwo.py
@@ -1,2 +1,17 @@
-""" Python program to add two numbers """
+""" Maximum of two numbers in Python """
 
+def maxNum(a,b):
+  if a > b:
+    return True,a
+  elif b > a:
+    return True,b
+  else:
+    print("Both are equal!")
+    return False,b
+
+num1 = int(input("Num 1: "))
+num2 = int(input("Num 2: "))
+flag,res = maxNum(num1,num2)
+
+if flag:
+  print(res,"is greater!")
