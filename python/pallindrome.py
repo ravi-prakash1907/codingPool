d3100ab7dbfd86cfcec6fdf66e024c6d1555ac6b
Jayanta Kumar Pal <paljayant1998@gmail.com>
2021-11-02 23:32:30 +0530

    Update pallindrome.py

--- python/pallindrome.py
+++ python/pallindrome.py
@@ -14,3 +14,22 @@ if ispallindrome(N):
     print(N,"is a pallindrome number!")
 else:
     print(N,"is not a pallindrome number!")
+    
+    
+    
+    
+ """ Python Program to check Pallindrome string"""   
+def ispallindrome(string):
+    reversed_string = string[::-1]
+    status=1
+    if(string!=reversed_string):
+        status=0
+    return status
+
+
+string = input("Enter the string: ")
+status= isPallindrome(string)
+if(status):
+    print("It is a pallindrome string")
+else:
+    print("It is not a pallindrome string")
