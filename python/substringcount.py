b7be80628fd3f41254ef845eb215b4c9f2da154e
Jayanta Kumar Pal <paljayant1998@gmail.com>
2021-11-05 11:38:37 +0530

    Updated substringcount.py

--- python/substringcount.py
+++ python/substringcount.py
@@ -5,8 +5,10 @@ def count_substring(string, sub_string):
             count += 1    
     return count
 if __name__ == '__main__':
+    print("Enter a string: ", end = ' ')
     string = input().strip()
+    print("Enter substring: ", end = ' ')
     sub_string = input().strip()
     
     count = count_substring(string, sub_string)
-    print(count)
+    print("Count: ",count)
